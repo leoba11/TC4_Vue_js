c85debbedf0aaa8e8ab610f52530dfdbca5a35af
leoba11 <leobarri.2013@gmail.com>
2019-10-22 22:39:08 -0600

    codigo del profe

---
+++ cantones.php
@@ -0,0 +1,24 @@
+<?php
+ $request = json_decode(file_get_contents('php://input'));
+ switch($request->id) {
+     case 1:
+        $cantones = array(
+           array('id' => 1, 'nombre' =>
+'Central'), 
+           array('id' => 2, 'nombre' =>
+'Pavas') 
+        );
+        break;
+     case 2:
+        $cantones = array(
+           array('id' => 1, 'nombre' =>
+'Alajuela'), 
+           array('id' => 2, 'nombre' =>
+'San Rafael') 
+        );
+        break;
+     default:
+        $cantones = array();
+        break;
+ }
+ echo json_encode($cantones);
---
+++ provincias.php
@@ -0,0 +1,6 @@
+<?php
+ $provincias = array(
+    array('id' => 1, 'nombre' => 'San José'), 
+    array('id' => 2, 'nombre' => 'Alajuela') 
+ );
+ echo json_encode($provincias);
