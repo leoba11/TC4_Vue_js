c8f21922fcc4ac7b61406f68a39b67b570b2f29f
leoba11 <leobarri.2013@gmail.com>
2019-10-23 00:57:01 -0600

    distritos cambios

--- cantones.php
+++ cantones.php
@@ -1,22 +1,20 @@
 <?php
- $request = json_decode(file_get_contents('php://input'));
- switch($request->id) {
-     case 1:
-        $cantones = array(
-           array('id' => 1, 'nombre' => 'Central'), 
-           array('id' => 2, 'nombre' => 'Pavas') 
-        );
-        break;
-     case 2:
-        $cantones = array(
-           array('id' => 1, 'nombre' =>
-'Alajuela'), 
-           array('id' => 2, 'nombre' =>
-'San Rafael') 
-        );
-        break;
-     default:
-        $cantones = array();
-        break;
- }
- echo json_encode($cantones);
+   $request = json_decode(file_get_contents('php://input'));
+   switch($request->id) {
+      case 1:
+         $cantones = array(
+            array('id' => 1, 'nombre' => 'Central'), 
+            array('id' => 2, 'nombre' => 'Pavas') 
+         );
+         break;
+      case 2:
+         $cantones = array(
+            array('id' => 1, 'nombre' => 'Alajuela'), 
+            array('id' => 2, 'nombre' => 'San Rafael') 
+         );
+         break;
+      default:
+         $cantones = array();
+         break;
+   }
+   echo json_encode($cantones);
